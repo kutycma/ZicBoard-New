276af7ab7344acee8784ccbe621b1471a098f53a
kutycma <khacquang2010@gmail.com>
2024-04-01 14:34:42 +0700

    Update TelegramService.php

--- app/Services/TelegramService.php
+++ app/Services/TelegramService.php
@@ -1,91 +1,96 @@
-<?php //002cd
-if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
-?>
-HR+cPzo4kS1BG0BtOauiuo5PspjTE6G8Ruq48Q38ucn7Y3DBVk6I+YqM8Gncqvkadb5f+M/dwUID
-//3ZHAq8YCjFG7E95JvzZqMecCVA6gaxOdveLjpLSklfCPymyTs+KaD7AgQpDykpkn2ou2bZmjwL
-Uy8sRKPcHsJojYRUlWZqHqQnuXEomtPcffnCRQ7dkyN9dx/vz0+LDXxSVVpgITBtH2WQ/rH65tKu
-y7172UcViWdEMhNIID49nGK2+jitAL9nJaX/azOu9rtRGPUHawQ+Pw0lS4Rwp5OOweJBJdPH4z/G
-+atzSuVzaUkjhs6LabYjun9K2FzN/JGi9MDE9TXZTuDJj5ByvVPV1/La1cZFgnI76dDC2eIkqbfJ
-LmX7x6OcdFjBpbKlsSt60ayYVKWNsEmRVV5qERajCB+KHPvd8cJ1/Q/j719J5TVoiRoytWflgAO9
-kaLI9EEAEWX/9g0n+zljeN76JW2BCXbg9vCmzFcx36kPFjgP936N3x8ShEIIe2zjvdUol/0h9Oa+
-75EWhF0/5aIAr08275v5e/rwPgd2CB/qxh+KUzoNfln119VB95wZjO+lNtZomI+yZsfWHJ7P5YCN
-MNUp92/8t2kZfq3kn1bF2yzWS9gU+92YVW35MONRVsi8jo1A1DS5MawL9xNsUL5JBYVApj5GVCJh
-JG2Ci1nZk/ZhkEdzkYHHaZDnaVIV7fSwsDXOeIn5XRNH+/aqhDYJu0HRB2LtvUJvZZ5Tdv8vEzNO
-Z07GXIk2CJhlLQMKiIdCS+WNEXgSn5GLSxJqqh2TZjLlQkDQI0U+e7mzKEndrT15gqauNIOMpoIT
-CY+IF/YHKRLnr61eeh04aPUObOGD8dJS7zZp+orrEM2HIHoUuHhO286EYKe/dZWO8lwMyFaWGfsf
-2i9ofRH1TtaCHaEmMoVXDkBqcKKbjOtalpDWekG2BHIdEUOptwtyPYPsxDrlcUAbOVwJ0oJ0mYti
-WQ7N+Di6/ZQBgh+hkstR8MVyq+5nP6dFEMU0m3Tiyetb/9iISDrUhyasSCa6aZerENN2xpD8VAnx
-I6UAlrANOFgdlmtokS2cX44UkEF7b2urODBMD9+rkMXPZAssDQiqKN67gfDG3ZfKVUIUyfOcDOeK
-L1cjDr8k/WUuwvgI1gSjifwq+Mfa3esl4zINO4CCc15CQIc2p4NQ28Q7C3r+NaBQjXD5r+S5B9X6
-yXwALusdT15VNE6ux6TSybWHqfNIviIhiaiK9dV1SZcOqe//t2g14Rt8C1EsX9iY1nYnuGpFKsn3
-tBGoey6f3a2keKTlzCZ56ykYodUXUKSO7K7Ol+M+oq5it2PuZQ7M82w36BLTFvqDESXARKH/OvPZ
-B+k/gm6P8+VJReaY5GpiEuorIEsfS4vCvuusWXy3jCMmo/AFpYZP40gaI5/0fxPrN/9K803nlFFX
-js8prgme3mltz5R39Ae6wBcWA8ap7FJ4/52eFQz9irNbNtF0h1NChQy2KOZJN6o8bFtDqMtb6oaW
-nMrA0bdCLFyLqAGvNgV+ZRcbEhoDuFpsSRHRb5jXLva3RBQeaTgtAM5SdBBzBzGzfCzIV1ykdda1
-Mmm8KmZtBaR02m5GrUeMs2h1WiPXzofR+D4ba9z69tbk0/Yv6tD9/eIXkUd+Nnn+XH9uph1KT5+E
-7CIX8jPDfQenZzOH4pC6R6tHV3ipp8MiBk239uC81SPJ/+u/rtHiQrjFInuWEyr+ryr2ajtiY4Uo
-Mr6KOcmA3L7XLfzyO4r1p/JftMiPDX/2youNiufNVNP9pcV68AFJRljzXnRLRtHbtdukKu1n/pSu
-ZofDFNapEue5dlaPCUeNQ5qwuKMqePH5tgCJ2aKDshHsex8ZVpNINL5zbMVVeihZ0sFEO24VgbJ6
-ZgbHSERwXtWokRG1EdxPGT1wysovVrQSku4OMEeR8a3ME6SW+Y4uGJ/T3ZG8OPFgbKVCq/j+693a
-5CfeA+0G0XNsm9IJggwMhWdQzO+A/b2pZN0L0KQywIOfyNYQIZw451/dlSpfWMWXVD7t9hehDgW7
-+7nENo3/XnfC6FDsIDqeNgJMJfSNjxJhwYfyGhAopD4V+gMDwiWIHdXi2ooOFYKz1ZCPcaNWGqQw
-lc5Xt6Tq1w+72bSYBSoUsAoJ2p3Y9AQMgDcwdAdfl1qC3S1Vrg2ErFOMFumf23tXl31xpHzPkdBL
-EFazkAtdWetct1Org1N4beF0UoEa9dRlBNxDKKzhMQv5AKZO2ZZLGLEDtqlz8cCMpcjgR9dNY4Iw
-/R3bc4hNd+f3pvSM2eNc8CLwT5n7AY75fNs3fun4Ayh8w/Dq6JUNBUxz62/aR7JhqlPffpSiNhQI
-1ZZmrnxrsMeBPHocBlkqtzHEbXHPV7s4Fg2BGkwuFSP0FtuOZIIP8Wo5zp86jHqZ22NERIJCqhHd
-5xo2zLBykY3b0kIPNznZK8/TXbJQYQLcO+iOKd7Ca8iCGS6LLN4apNQ/yBkQgNtU2Y9mK4mLa0i0
-e5y+tczMnvKXfC2tQK5p4ObJo7bMoRYn1Y9Ue/F2/iL/gCnF4nG9uNPcMFJQGtYV+685PV/8/vE2
-Jom2i1oCBLbtt7zP/51GiVb3mDwOepsmL9sl/XJ8G9TFwMCoEqQnm5OjAY4ztrPzBnFKlTgIQu1N
-Crm0fE8xNytbKRQbOvloMGJ9qFk1hCBKG3sxIfSjlZ+XVDAyWKPpXFaxD0xPBGkO4JK+AxVIsqrB
-iDkZV0A9g5MTR9Mu7o1eSBdkrPRof1meFsZ5Avd6sxAdtaNfgzROG6cKWDp+A0VfuqWUkorihX/N
-KV/e03uFydL0grGDKnpdxuxWlqn5Az4/ajGYJ7GpM6NzQO/DRMZt97eK0MURe06dZ+JFagKdALu0
-6a5mzaptkjGdG86PlC+8jpwE7lRatHoqkUkh+9DusE8RHjPPIVLb0d6cILIsUxte3BU7jvcWEXdB
-EHAHbewXgkdES47PQ2N/yUl9mg8Cls9J5Ww3GEfTlAt2nkfZ4mebgMOJE+O1+ZX1Dv65gwNz6qDP
-L+c8UWM5HLArZ7uaMVJTC06OHOEidW0uT2A8RyVCoUKsuI0+E+dbMDVCA3MLo05V3axx9ZTRItYt
-foQqMdgjy6OZXx99LBiO0KhqyhOOLa8hx8WGFInnN8p0qmifEG9H9mk1l7Oir3Wq2tdZ7RuCY/B2
-HaRCdyeM1qwxJ4taxoDdGEkkFeGHubXK2v3xwX+193IV+6n25lWhfsypxZsfZbpXaLtJ2fACUOym
-yegVXTdmSlgq58ydVknh3sNiBJZv9jvYPqbF4OZ0+8T0BAKs3Igr7r1sbFVI1NcRZ2mMdKRulE5W
-AValEkbaZzH1lLV93pQuK6XwFKXm4mefKnkwlG7Z6QW9kCI0/fwXdoWFd2LfbUG3kch6JLhngTzT
-YErzD0+IwIlcrEb8wqJlQEZWudYoPRuT/bhQmagkeTpOLLav7wR3L2Xpg7SY8tpQ/WLR9pWVduFE
-aV0eHdRu36qu4a2ypEaosscgzOfi9BRSyk+LAmzTo2Hf6qU9CKcbYxlvfqSpVh/paScmYFPbE/Yw
-ZS2/mQw5NcjT5cnRv+vaqG4J90jPjwmauh9A/k3mSaPUutRdzibSWMRLgcMby0yOAr044EeBFTaT
-EC7wUNftrbq9G2iQ6Lei2guazyrrkT1vSkK2Ltrjx9kTumNIP4lz5xaoXKzlG3VtStYlHqNLIAHs
-16X0a4+02zFCt7bt7cOzoDfiKWE574jVZOcP/XYuvcrajnOBCSWiHxqd8sTtVyD8RvuqpFuRSUtO
-ZgKLNoTF5X3Jg/xnA+6VULEpOmKAhGw2l4Ay98d6f/QTNxHJt474upTMAXOYSlWY51nBq01kLF5o
-NLk2ql1nu/EaU+7qAjmkXiaRrVgZrG+SxfbwiI+J3MjLxNUfhtvJQC20oAecn8qbRayLjjDjcniA
-5JASq8yW7SXnAwL2HKR9iMR4piPpjeenRaTr1XtSQG7RxQm0knzV4qsLKcGCSIJDpp7Sma++xVHk
-ChatIRGVOn5k/gtyduiLidBggOBJPircC5oXxzEAaDFqdr8HsZV4h8w02ozODcn376sxL3QukYDw
-MX9pKvXJr3XBSrcAxVGf5fB3CVcMQrp7MEIZEOIIs7+ZbmJ/51bm63zixJS/jHXTqORebNbd/FHI
-KXLV8f2Yl9/WjHZEI46j/F9dqCdwwLLVYNlAhs20cjJI61P/2N87dFhpmkCN7TRknHALsQmivXbS
-vjW7HhjYo1tKRpR+JbeC1YVDqfNYagWSR2Wb+EGkTvaYFXazAA5tOSvvOTVBOdowkJbs5TowTWzg
-vyCDaBQfRJlKvKBW4tOf1LRwcY4pthBT1WT8RyuQT9Sv83sdQCdRWAVt0ecCb9e1YDyW7p0BOfyz
-XaTTjo1VNxsYN7dfHLNNZSE9JNrz7vxVmHBVNx1qpG9XlhOVAKdmVv6e5F5LxmG7n7gcNyyaUWk1
-gLq8ZXkCEQbI7NcYEePhCROBDSKanCIiIPGsL6I/IUCWBuVTvNJQxhta26X5LzZ7JekbV8V9usp3
-7T2yV65PeM7WrPblxUfjftNyslWaqwwGk4LLQhvdKOom8rHxJ6KaTFuG66gDReWwEnMkACnilStX
-Z6k3Ar+MEURIkIpbTKWV7I9L+ASYh/mT0QE7krdlSSRHLhSgLeXvcvJr9WLap3hQnZrVZwdTg5a8
-W4m66ZutcMvjK4I7Z5JoM4xqonvAu3txMM7o/2I6Y0k5O6vAr527a0H+Udt6v4gg3l8+HNroczZC
-lEIXapqFtotR0TnQu55xFHWLXEaO4yGN559otsPaIatmZRn51C/3tUGs/yR6N0bm02vvHilrV/RU
-hC6FOT92VSRaBlKbBRT3vd+izJb7Ho321Dk+LKY46vqjPAHMb6B+Xq2kJc3zAETQnJg7zbvDjPoo
-l2AFmB5drfZCv47R9/daL+tFB96PoM/XBZeOzb0CcMsiL1e2Lg/cJk14feDN/iGroChp1nD+aIiA
-PL6qslwZnZr43T81/3WCNumrAzCpE5/F1fvyOMq3moasV9QcGJDQ62Nz15xAXJ/VNIBgQqszzEGJ
-iKJoGZ5lFKtsAj/AFW9jrNBJv30BUIDaxq0BnIQewtyGWnbyarorFkmdc3L0n09tN6f0uEttMgML
-Zx/GT7De45pbtL0+ZMN/anDKyVAomGMuy9XU5qq2KqgjqY4CnpPD3oDKpcFBjDOoOjREldqP8JH6
-+6SB7sBq7FsVzfVRt9N8C0dJqo2/oWDuwoP1RFEab6hmU7eovggaRM48wiQgJ3S80ITuG/v+zFZs
-Ir1o2s8B0XAgVOneAwcti/AarGjro0gwNDuQ5Fo0XS6gDb9RPEb+5DLXewJ9u1mo3lJBVcA415V4
-VkYJvBRhSTQLZGmIuwXbb6q2bSn5qC3Ddb0uap/Yh6NiKjpH7iOiuwiMLNzbDjvuymGMOo9fwNQc
-V1gWYPVMES1szFy/LxKavXYHOImOoBz7BcPeKk14ETaeAC8LCHN5QgmvNgOnWFauhZMaHhsdSdgT
-4d4lSUx7zCi2KVoPDYVjYjbuy19bSAeKQwrJ7UAXiwI5sDzHE59EtpXBra4Hqvmdw0cnsOyMWHSp
-wCvDl0nmqAK1LTruFsr+vA45u0H1pXH25oA5JJv1IHxPa82ejmCEmZFlcp2MOR1f9qP/zaps9Bcp
-XvXvioXhy0MRCz8ZOJD3P7KTKhQBeeAYifLaVf0IBKw2CGcDG6TCdYPzM7+HfBItanjiFJFddgsP
-ZVKYhKhlVH5KjAnV2/QC0eNnIeNQ/pT9j+W+zUEg+GrWEiw4byyU48K8gZcI0x3sBS3/9/+vRBoY
-Aq+r2DcgnISKbLdj6DbeZUefhBQdU3/9n2eEQ16YmOkj3osjl1GGR5wC5ImA8dr5Io00m7R9SRUP
-vXcfT5CKHY0FHB9uFky62Nj+lflu2elgig/TaMw7iCVGcvir/fudhhJwY+oHcMrXNXog4X/5PkPm
-ZwMrfWB3FJv79qAl+ssGAKte0034WcoZC48Gk/OtuBLb4uq6aAthct4M5VTlqai5vBFtDVeaptVf
-ltzL7m1kTGd/HH18tNOtgEEPFu+9kXHIV0oDDQqQFQlEuKBjnC6pe5eNEiytqVdYzmVZVjsD9KaV
-je76riQ30SM1pbukV7xdN7XDfuXmKPzxxFGdu1XcYoeAOpwfaoed4y3UejY/HjJM/aXLbfAPwvnL
-lCVAmkn3wAWWaks12PCT6lO82gvx/NJSvm9xcBZpOn2o32egwDtECPY8fEPzu8QapR37FT8DEXed
-EObDCHiNJuSCjJE0u0EZd4Dd70Y5LeYwM9CZdt5uprsr+AYvDTg0yc5o+aPE+D1TC/CNg2ARrc/i
-IH71Zq/DDowplSrKrQ2Okdr/mTs84OTqPDp4cy8MAknBWXy9hy6bjpZfkjJrj+gz6QtFEe6/jOKw
-rdZNNhEGzrOq/Oa8wQEteb2KpEHWyAANnupkAnG+SeP4jHVFw2BTX2pohfMzIwLU25jsFVHfzrrb
-occOIWaLZYLvCpVyMsujKHz1JH28hJzlnQ1QToMtXvzvK7WVtcO48EiEYfl+3AkBXzSpuLErum76
-ftp2m5mvQKrJlFzb6Zlv
+<?php
+namespace App\Services;
+
+use App\Jobs\SendTelegramJob;
+use App\Models\User;
+use \Curl\Curl;
+use Illuminate\Mail\Markdown;
+
+class TelegramService {
+    protected $api;
+
+    public function __construct($token = '')
+    {
+        $this->api = 'https://api.telegram.org/bot' . config('v2board.telegram_bot_token', $token) . '/';
+    }
+
+    public function sendMessage(int $chatId, string $text, string $parseMode = '')
+    {
+        if ($parseMode === 'markdown') {
+            $text = str_replace('_', '\_', $text);
+        }
+        $this->request('sendMessage', [
+            'chat_id' => $chatId,
+            'text' => $text,
+            'parse_mode' => $parseMode
+        ]);
+    }
+
+    public function approveChatJoinRequest(int $chatId, int $userId)
+    {
+        $this->request('approveChatJoinRequest', [
+            'chat_id' => $chatId,
+            'user_id' => $userId
+        ]);
+    }
+
+    public function declineChatJoinRequest(int $chatId, int $userId)
+    {
+        $this->request('declineChatJoinRequest', [
+            'chat_id' => $chatId,
+            'user_id' => $userId
+        ]);
+    }
+
+    public function getMe()
+    {
+        return $this->request('getMe');
+    }
+
+    public function setWebhook(string $url)
+    {
+        return $this->request('setWebhook', [
+            'url' => $url
+        ]);
+    }
+
+    private function request(string $method, array $params = [])
+    {
+        $curl = new Curl();
+        $curl->get($this->api . $method . '?' . http_build_query($params));
+        $response = $curl->response;
+        $curl->close();
+        if (!isset($response->ok)) abort(500, '请求失败');
+        if (!$response->ok) {
+            abort(500, '来自TG的错误：' . $response->description);
+        }
+        return $response;
+    }
+
+    public function sendMessageWithAdmin($message, $isStaff = false)
+    {
+        if (!config('v2board.telegram_bot_enable', 0)) return;
+        $users = User::where(function ($query) use ($isStaff) {
+            $query->where('is_admin', 1);
+            if ($isStaff) {
+                $query->orWhere('is_staff', 1);
+            }
+        })
+            ->where('telegram_id', '!=', NULL)
+            ->get();
+        foreach ($users as $user) {
+            SendTelegramJob::dispatch($user->telegram_id, $message);
+        }
+    }
+
+    public function sendMessageToUser($message,int $userId)
+    {
+        $user = User::find($userId);
+        if ($user && $user->telegram_id) {
+            $this->sendMessage($user->telegram_id, $message);
+        } else {
+            throw new \Exception('User not found or user does not have a Telegram ID');
+        }
+    }
+    
+}
